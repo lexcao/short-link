aad8ceaddbf2df5c78687a0663adc52e4683b9b7
Lex Cao <lexcao@foxmail.com>
2020-07-14 18:20:35 +0800

    :rocket: Support kotlin-mpp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("multiplatform") version "1.3.72"
 }
+
 group = "lexcao.github.io"
 version = "1.0-SNAPSHOT"
+
+val ktorVersion: String by project
+val kotlinHtmlVersion: String by project
 
 repositories {
     mavenCentral()
@@ -13,34 +15,60 @@ repositories {
     mavenLocal()
 }
 
-val ktorVersion = "1.3.2"
+kotlin {
 
-dependencies {
-    testImplementation(kotlin("test-junit"))
-
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("io.ktor:ktor-server-core:$ktorVersion")
-    implementation("io.ktor:ktor-html-builder:$ktorVersion")
-    implementation("io.ktor:ktor-jackson:$ktorVersion")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.1")
-
-    implementation("io.github.microutils:kotlin-logging:1.7.9")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.withType<Jar> {
-    archiveVersion.set("")
-    manifest {
-        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        tasks.getByName<Jar>("jvmJar") {
+            archiveVersion.set("")
+            manifest {
+                attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+            }
+            doFirst {
+                from(configurations["jvmCompileClasspath"].map { zipTree(it.absolutePath) })
+            }
+        }
     }
-    configurations["compileClasspath"].forEach { file: File ->
-        from(zipTree(file.absoluteFile))
+
+    js { browser() }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("io.ktor:ktor-server-netty:$ktorVersion")
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-html-builder:$ktorVersion")
+                implementation("io.ktor:ktor-jackson:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinHtmlVersion")
+                implementation("io.github.microutils:kotlin-logging:1.7.9")
+                implementation("ch.qos.logback:logback-classic:1.2.3")
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+                implementation("org.jetbrains.kotlinx:kotlinx-html-js:$kotlinHtmlVersion")
+            }
+        }
     }
 }
 
+/*tasks.getByName<KotlinWebpack>("jsBrowserProductionWebpack") {
+    outputFileName = "output.js"
+}*/
+/*tasks.getByName<Jar>("jvmJar") {
+
+    *//*dependsOn(tasks.getByName("jsBrowserProductionWebpack"))
+    val jsBrowserProductionWebpack =
+        tasks.getByName<org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack>("jsBrowserProductionWebpack")
+    from(File(jsBrowserProductionWebpack.destinationDirectory, jsBrowserProductionWebpack.outputFileName))
+*//*
+}*/
